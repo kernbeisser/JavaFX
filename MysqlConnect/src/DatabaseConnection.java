7dcbe46f8e4fbb7fa919663af803efd19625107e
kernbeisser <kernbeisser@tom-bonn.de>
2025-12-06 20:46:42 +0100

    Remove comments

--- MysqlConnect/src/DatabaseConnection.java
+++ MysqlConnect/src/DatabaseConnection.java
@@ -11,16 +11,13 @@ public class DatabaseConnection {
         String databaseName = "mysql";
         String databaseUser = "...";
         String databasePassword = "...";
-        // String url = "jdbc:mysql://localhost:3306/" + databaseName;
+        
         String url = "jdbc:mysql://localhost:3306/" + databaseName;
 
         try {
-            // Class.forName("com.mysql.jdbc.Driver");
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
-            IO.println("bin drin...");
         } catch (ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException e) {     
-            IO.println("Argh...");       
             e.getMessage();
         }
 
