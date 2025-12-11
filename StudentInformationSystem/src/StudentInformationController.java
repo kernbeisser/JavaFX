831c6a24ccd4612d2502166e660e093bbd4a6d39
kernbeisser <kernbeisser@tom-bonn.de>
2025-12-11 14:27:29 +0100

    ComboBox and clear Button

--- StudentInformationSystem/src/StudentInformationController.java
+++ StudentInformationSystem/src/StudentInformationController.java
@@ -19,6 +19,11 @@ import javafx.scene.control.ToggleGroup;
 
 public class StudentInformationController implements Initializable{
 
+    private StringBuilder sb;
+
+     @FXML
+    private Button btnClear;
+
     @FXML
     private Label lblStatus;
 
@@ -44,7 +49,7 @@ public class StudentInformationController implements Initializable{
     private CheckBox cbVolunteerWork;
 
     @FXML
-    private ComboBox<?> cmbSunjects;
+    private ComboBox<String> cmbSubjects;
 
     @FXML
     private RadioButton rbBusinessAnakyst;
@@ -58,7 +63,7 @@ public class StudentInformationController implements Initializable{
 
 
     @FXML
-    private TextArea tadisplay;
+    private TextArea taDisplay;
 
     @FXML
     private TextField txtAddress;
@@ -86,7 +91,8 @@ public class StudentInformationController implements Initializable{
 
     @FXML
     void onDisplay(ActionEvent event) {
-        StringBuilder sb = new StringBuilder();
+
+        sb.append("Personal Details:\n");
 
         sb.append(txtFirstName.getText()).append("\n");
         sb.append(txtAddress.getText()).append("\n");
@@ -96,7 +102,38 @@ public class StudentInformationController implements Initializable{
         sb.append(txtPostalCode.getText()).append("\n");
         sb.append(txtEmail.getText());
 
-        tadisplay.setText(sb.toString());
+        sb.append("\nAcademcis\n");
+        if (rbComputerScience.isSelected()) {
+            sb.append("Computer Science\n");
+        } else if (rbBusinessAnakyst.isSelected()) {
+            sb.append("Bussiness Analyst\n");
+        }
+        
+        sb.append("Subjcts:\n");
+        sb.append(cmbSubjects.getValue());
+
+        sb.append("\nActivities\n");
+        if (cbBasketball.isSelected()) {
+            sb.append("Basketball\n");
+        }
+        
+        if (cbVolleyball.isSelected()) {
+            sb.append("Volleyball\n");
+        }
+        
+        if (cbSwimming.isSelected()) {
+            sb.append("Swimming\n");
+        }
+        
+        if (cbStudentsCouncil.isSelected()) {
+            sb.append("Student's Council\n");
+        }
+        
+        if (cbVolunteerWork.isSelected()) {
+            sb.append("Doing volonteer work\n");
+        }
+                
+        taDisplay.setText(sb.toString());
     }
 
     @FXML
@@ -107,19 +144,34 @@ public class StudentInformationController implements Initializable{
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         ToggleGroup tglGroup = new ToggleGroup();
+        sb = new StringBuilder();
 
         tglGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 
             @Override
             public void changed(ObservableValue<? extends Toggle> arg0, Toggle arg1, Toggle arg2) {
-                RadioButton slectedRB = (RadioButton) tglGroup.getSelectedToggle();
-                lblStatus.setText(""+ slectedRB.getText());
+                RadioButton selectedRB = (RadioButton) tglGroup.getSelectedToggle();
+
+                lblStatus.setText(""+ selectedRB.getText());
+
+                if (selectedRB.getText().equals("Computer Science")) {
+                    cmbSubjects.getItems().clear();
+                    cmbSubjects.getItems().addAll("Java", "Python", "C++");
+                } else if (selectedRB.getText().equals("Business Analyst")) {
+                    cmbSubjects.getItems().clear();
+                    cmbSubjects.getItems().addAll("Process Mangement", "Supply Chain", "Resource Planing");
+                }
             }
             
         });
 
         this.rbComputerScience.setToggleGroup(tglGroup);
         this.rbBusinessAnakyst.setToggleGroup(tglGroup);
+
     }
 
+    @FXML
+    void onClear(ActionEvent event) {
+        taDisplay.clear();
+    }
 }
